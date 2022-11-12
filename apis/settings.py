21d087de0311a929eaa47ff667278c76d7689aa5
whoisdinanath <whoisdinanath@gmail.com>
2022-11-12 17:51:44 +0545

    added allowed host for custom domain

--- apis/settings.py
+++ apis/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-u!3qhd!qff7hgnqf2rh*(p$=kk6+$g(imhas%gq+1v_nnnvw)r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["nepal-api.onrender.com","127.0.0.1","localhost"]
+ALLOWED_HOSTS = ["nepal-api.onrender.com", "127.0.0.1",
+                 "localhost", "api.eduspotnp.com", "rasifal-api.eduspotnp.com"]
 
 # Application definition
 
